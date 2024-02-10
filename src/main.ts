fa1d808260159c340b2587bcc34ac94f639001c9
nima <nimakalhor25@gmail.com>
2024-02-10 11:33:46 +0330

    #

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
+import { ComputerModule } from './computer/computer.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
+  const app = await NestFactory.create(ComputerModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
 }
